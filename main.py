a433ae4e208c2ced70ccc09e6a167b1bddfbae48
Faizul Amali <faizuloke@gmail.com>
2019-11-16 12:31:22 +0700

    Simpan nomor induk jika sudah ada

--- main.py
+++ main.py
@@ -4,33 +4,37 @@ from app import logger
 
 api = Pedatren()
 
+
 def allData():
     data = api.all_pelajar()
     temp = []
     temp.append([
         'NOMOR INDUK', 'UUID', 'NAMA LENGKAP', 'ID PENDIDIKAN',
         'ID LEMBAGA', 'KELAS', 'JURUSAN', 'TANGGAL MASUK LEMBAGA',
-        ])
+    ])
     for i in data:
         data_pelajar = (
-            None,
+            i.get("pendidikan").get("nomor_induk"),
             i.get('uuid'),
-            i.get('nama_lengkap'), i.get('pendidikan').get('id'), i.get('pendidikan').get('id_lembaga'),
-            i.get('pendidikan').get('kelas'), i.get('pendidikan').get('jurusan'), i.get('pendidikan').get('tanggal_mulai'),  
+            i.get('nama_lengkap'), i.get('pendidikan').get(
+                'id'), i.get('pendidikan').get('id_lembaga'),
+            i.get('pendidikan').get('kelas'), i.get('pendidikan').get(
+                'jurusan'), i.get('pendidikan').get('tanggal_mulai'),
         )
         temp.append(data_pelajar)
     cetakExcel(temp)
-    
+
+
 def updateNomorInduk():
     book = xlrd.open_workbook("data_siswa.xlsx")
     sheet = book.sheet_by_index(0)
     for i in range(1, sheet.nrows):
-        induk = sheet.cell(i,0).value
-        uuid = sheet.cell(i,1).value
-        nama = sheet.cell(i,2).value
-        id_pendidikan = sheet.cell(i,3).value
-        id_lembaga = sheet.cell(i,4).value
-        tanggal_masuk = sheet.cell(i,7).value
+        induk = sheet.cell(i, 0).value
+        uuid = sheet.cell(i, 1).value
+        nama = sheet.cell(i, 2).value
+        id_pendidikan = sheet.cell(i, 3).value
+        id_lembaga = sheet.cell(i, 4).value
+        tanggal_masuk = sheet.cell(i, 7).value
         updatePelajar = api.updateInduk(
             uuid=uuid, induk=induk, id_lembaga=id_lembaga,
             id_pendidikan=id_pendidikan, tanggal_masuk=tanggal_masuk)
@@ -39,16 +43,19 @@ def updateNomorInduk():
             print(sukses)
             logger.info(sukses)
         elif updatePelajar == 400:
-            duplikat = "Terdapat duplikat nomor induk Nanda : {} Dengan Induk : {}.\nSilahkan diperbaiki".format(nama, induk)
+            duplikat = "Terdapat duplikat nomor induk Nanda : {} Dengan Induk : {}.\nSilahkan diperbaiki".format(
+                nama, induk)
             print(duplikat)
             logger.error(duplikat)
         else:
-            gagal = "Update Nomor Induk Nanda : {} Gagal\nSilahkan cek Log".format(nama)
+            gagal = "Update Nomor Induk Nanda : {} Gagal\nSilahkan cek Log".format(
+                nama)
             print(gagal)
             logger.error(gagal)
 
+
 if __name__ == '__main__':
-    print '''1. Ambil Data Dari PEDATREN\n2. Update Nomor Induk'''
+    print ('''1. Ambil Data Dari PEDATREN\n2. Update Nomor Induk''')
     try:
         pilih = input("Silahkan Masukkan Pilihan : ")
         if pilih == 1:
